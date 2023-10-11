813e3ee1cbbb9d91f6b7e29cfbaf68864a925e0b
Vincenzo140 <vincenzo.amendola14@gmail.com>
2023-10-11 00:43:09 -0300

    last

--- main.py
+++ main.py
@@ -5,32 +5,32 @@ import uuid
 
 app = FastAPI()
 
-# Conecte-se ao MongoDB
+
 client = MongoClient('mongodb://localhost:27017/')
-db = client['pessoasdb']  # Nome do banco de dados
-collection = db['pessoas']  # Nome da coleção
+db = client['pessoasdb']  
+collection = db['pessoas']  
 
 class PessoaCreate(BaseModel):
     apelido: constr(max_length=32)
     nome: constr(max_length=100)
-    nascimento: int  # Alterado para inteiro
+    nascimento: int  
     stack: list[str] = None
 
 class PessoaResponse(BaseModel):
     id: str
     apelido: str
     nome: str
-    nascimento: int  # Alterado para inteiro
+    nascimento: int  
     stack: list[str] = None
 
 @app.post('/pessoas', response_model=PessoaResponse, status_code=201)
 def create_pessoa(pessoa: PessoaCreate):
-    # Verifique se o apelido já existe no banco de dados
+    
     existing_pessoa = collection.find_one({'apelido': pessoa.apelido})
     if existing_pessoa:
         raise HTTPException(status_code=422, detail="Apelido já existe")
 
-    # Insira a nova pessoa no MongoDB
+
     new_pessoa = {
         'id': str(uuid.uuid4()),
         'apelido': pessoa.apelido,
@@ -44,7 +44,7 @@ def create_pessoa(pessoa: PessoaCreate):
 
 @app.get('/pessoas/{id}', response_model=PessoaResponse)
 async def find_by_id(id: str):
-    # Encontre uma pessoa no MongoDB com base no ID
+    
     pessoa = collection.find_one({'id': id})
     if not pessoa:
         raise HTTPException(status_code=404, detail="Pessoa não encontrada")
@@ -53,7 +53,7 @@ async def find_by_id(id: str):
 
 @app.get('/pessoas', response_model=list[PessoaResponse])
 def find_by_term(term: str = Query(..., min_length=1)):
-    # Pesquise pessoas no MongoDB com base em um termo
+
     results = []
     cursor = collection.find()
     for pessoa in cursor:
@@ -63,6 +63,6 @@ def find_by_term(term: str = Query(..., min_length=1)):
 
 @app.get("/contagem-pessoas")
 def count_pessoas():
-    # Conte as pessoas no MongoDB
+    
     count = collection.count_documents({})
     return count
