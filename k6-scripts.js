ba061488d8431bba01ea950dccea9a16e50e67b5
Vincenzo140 <vincenzo.amendola141@gmail.com>
2024-04-04 13:53:05 -0300

    feat: Added .env.dev file and k6-scripts.js.
    Added .env.dev file and k6-scripts.js.
    
    This commit introduces two new files:
    
    .env.dev: This file contains environment variables specifically tailored for development purposes.
    k6-scripts.js: This file contains scripts for load testing using k6, facilitating performance testing within the development workflow.

---
+++ k6-scripts.js
@@ -0,0 +1,15 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  var server_list = ["localhost:8000", "localhost:8001", "localhost:8007"];
+  var endpoint_list = ["/cpu", "/data", "/ram", "/chain", "/dados_invalidos", "/desculpas"];
+  
+  server_list.forEach(function(server) {
+    endpoint_list.forEach(function(endpoint) {
+      http.get("http://" + server + endpoint);
+    });
+  });
+  
+  sleep(0.5);
+}
